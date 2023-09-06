7ce1bc38518b1a4d8d19420384ba59463f15862d
Darin Gregg <daringregg801@gmail.com>
2023-09-06 08:53:59 -0600

    JS Operators

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 console.log("Hello World!\n==========\n");
 console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
-
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n");
+  let a= 20;
+  let b= 4;
+  let add= a+b;
+  let minus= a-b;
+  let multiply= a*b;
+  let dividing= a/b;
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
+
+
 // YOUR CODE HERE
+
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(isPresent + str)
 
 // YOUR CODE HERE
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+console.log(!isPresent2)
+
 
 // YOUR CODE HERE
