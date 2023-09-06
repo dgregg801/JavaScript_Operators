e78a7d912f312040cb9fdbe2453f546cfd06292a
Darin Gregg <daringregg801@gmail.com>
2023-09-06 09:40:06 -0600

    updated

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n");
-  let a= 20;
-  let b= 4;
-  let add= a+b;
-  let minus= a-b;
-  let multiply= a*b;
-  let dividing= a/b;
+
+console.log("Follow the steps in the README.md file to complete the exercises(:\n==========\n");
+ 
+
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
+let a= 20;
+let b= 4;
+let add= a+b;
+let minus= a-b;
+let multiply= a*b;
+let dividing= a/b;
+
+console.log( add, minus, multiply, dividing)
 
 
 
@@ -24,6 +28,15 @@ let isPresent = true;
 let firstName = "Frodo";
 let lastName = "Baggins";
 
+/*
+What is the value of: num + str? "1111"
+What is the value of: num + str2? "11eleven"
+What is the value of: num + isPresent? 12
+What is the value of: firstName + num? "Frodo11"
+What is the value of: isPresent + str? "true11"
+What is the value of: firstName + lastName? "Frodo Baggins"
+*/ 
+
 console.log(isPresent + str)
 
 // YOUR CODE HERE
@@ -35,7 +48,19 @@ let str3 = "5";
 let str4 = "five";
 let isPresent2 = false;
 
-console.log(!isPresent2)
+console.log(0 != false)
+
+/* 
+What is the value of: val == str3? True
+What is the value of: val === str3? False
+What is the value of: !isPresent2? True
+What is the value of: (“eleven” == str4 && val >= str3)? False
+What is the value of: (!isPresent2 || isPresent2)? True
+What is the value of: 0 == false? True
+What is the value of: 0 === false? False
+What is the value of: 0 != false? False
+What is the value of 0 !== false? True
+*/
 
 
 // YOUR CODE HERE
